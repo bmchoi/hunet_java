f3fecfafd931aab41103a42b7711d57735be268d
bmchoi <cbm1003@gmail.com>
2018-06-21 22:51:31 +0900

    test

--- src/com/hunet/java/ch05/Example04.java
+++ src/com/hunet/java/ch05/Example04.java
@@ -4,7 +4,7 @@ public class Example04 {
 	public static void main(String[] args) {
 		for(int i=1; i<=5; i++) {
 			for(int j=1; j<=i; j++) {
-				System.out.printf("*");
+				System.out.printf("#");
 			}
 			System.out.println();
 		}
